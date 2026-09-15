1b56cad44450213d30f77c61f7529c6f3bacf08f
Md Sabbir Hossain <hossainsabbir309@gmail.com>
2026-09-15 22:53:36 +0600

    update on hero text

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -23,7 +23,7 @@ const Hero = ({setDollars}:PropeTypes) => {
         <img src={shadow} alt="shadow" className="h-full w-full absolute top-0 left-0 rounded-lg"/>
         <div className="flex flex-col justify-center items-center text-white">
             <img src={bannerImg} alt="bannerImg" className="h-48 w-56 pb-5"/>
-            <h1 className="font-bold text-[40px]">Assemble Your Ultimate Dream  Cricket Team</h1>
+            <h1 className="font-bold text-[40px]">Assemble Your Ultimate Dream 11 Cricket Team</h1>
             <p className="text-[24px] opacity-70 py-3">Beyond Boundaries Beyond Limits</p>
 
             <div className="aura">
